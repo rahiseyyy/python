69fa2248f96ee94ae7023f5134e9f200dcf66df1
rahiseyyy <rkrithik557@gmail.com>
2025-10-08 13:52:38 +0530

    new.python

---
+++ python.py
@@ -0,0 +1,14 @@
+def add (a,b,c):
+    sum = a+b+c
+    print(sum)
+add(30,40,50)
+
+def greet(vibe):
+    print("Hello",vibe)
+greet("rahiseyy")
+
+def two():
+    print("hello world")
+two ()
+
+
---
+++ python1.py
@@ -0,0 +1,16 @@
+def add(a):
+    if a%2==0:
+       print("even")
+    else:
+        print("odd")
+add(67)
+
+def num(n):
+    for i in range(1,n,1):
+        print(i)
+num(50)
+    
+def num(r):
+    for g in range(r,0,-1):
+        print(g)
+num(50)
